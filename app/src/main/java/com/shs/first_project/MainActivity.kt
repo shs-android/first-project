740fecc733ba8b28a6b572d55e24ded99b447b55
Skyler <skyler.shin@kakaopaycorp.com>
2019-08-20 20:11:39 +0900

    first

---
+++ app/src/main/java/com/shs/first_project/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shs.first_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
