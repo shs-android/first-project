bc2670742ae235da3a9e438b14a15cde7f19bc52
leejj1345 <52849231+leejj1345@users.noreply.github.com>
2019-08-28 19:10:57 +0900

    Revert "메인작업"
    This reverts commit f137a82466fab19d9a097a25dedca156febca871.

--- app/src/main/java/com/shs/first_project/MainActivity.kt
+++ app/src/main/java/com/shs/first_project/MainActivity.kt
@@ -2,8 +2,6 @@ package com.shs.first_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
