38779de139ede6a95979519ba927c9003d9382f7
Dohyeon12 <52915526+Dohyeon12@users.noreply.github.com>
2019-09-03 19:18:05 +0900

    Update MainActivity.kt

--- app/src/main/java/com/shs/first_project/MainActivity.kt
+++ app/src/main/java/com/shs/first_project/MainActivity.kt
@@ -2,6 +2,8 @@ package com.shs.first_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
